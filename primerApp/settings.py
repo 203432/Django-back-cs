97d1d232b24d9fe556ce898bc3ee6a0f6f27558e
203432 <203432@ids.upchiapas.edu.mx>
2022-01-19 14:04:43 -0600

    create primerComponente and integrate model

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Componentes agregados
+    'primerComponente',
+
     #Librerias para el proyecto
      'rest_framework',
 ]
