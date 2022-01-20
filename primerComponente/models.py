344a8bca72253edeb1d0349eb55a38ceab47aa4a
203432 <203432@ids.upchiapas.edu.mx>
2022-01-19 21:24:24 -0600

    create serializers, view and urls on primerComponente

--- primerComponente/models.py
+++ primerComponente/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class primerTabla(models.Model):
+class PrimerTabla(models.Model):
     nombre = models.CharField(max_length = 50,null = False)
     edad = models.IntegerField(default = 0, null = False)
     created = models.DateTimeField(default=timezone.now)
